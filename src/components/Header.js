566e0922df696d78d4dda95afed0a621cc47b7a2
Hanna <hanna@oneplanetops.com>
2020-08-02 12:29:41 +0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+     <header className='header'>
+         <img src="https://image.freepik.com/free-vector/abstract-colorful-logo-design_1017-1083.jpg"/>
+      </header>
+    )
+}
+
+export default Header;
